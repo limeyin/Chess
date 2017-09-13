0f1bf870762ee483f13627e323d78b7da3e67639
lhaglund <leightonhaglund@gmail.com>
2017-09-12 22:14:22 -0500

    added king, pawn, queen, rook. changed bishop and rook

--- src/com/chess/engine/pieces/Queen.java
+++ src/com/chess/engine/pieces/Queen.java
@@ -1,35 +1,35 @@
 package com.chess.engine.pieces;
+
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Tile;
 import com.google.common.collect.ImmutableList;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bishop extends Piece {
-
-    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9, -7, 7, 9};
-
-    Bishop(int piecePosition, Alliance pieceAlliance) {
+public class Queen extends Piece {
+    Queen(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9};
 
     @Override
     public List<Move> calculateLegalMoves(final Board board) {
 
-       final List<Move> legalMoves = new ArrayList<>();
+        final List<Move> legalMoves = new ArrayList<>();
 
-       for (final int candidateCoordinateOffest : CANDIDATE_MOVE_VECTOR_COORDINATES){
-           int candidateDestinationCoordinate = this.piecePosition;
+        for (final int candidateCoordinateOffest : CANDIDATE_MOVE_VECTOR_COORDINATES){
+            int candidateDestinationCoordinate = this.piecePosition;
 
-           while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+            while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
 
-               if(isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffest) ||
-                       isEighthColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffest)){
-                   break;
-               }
+                if(isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffest) ||
+                        isEighthColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffest)) {
+                    break;
+                }
                 candidateDestinationCoordinate += candidateCoordinateOffest;
                 if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
                     final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
@@ -45,15 +45,16 @@ public class Bishop extends Piece {
                     }
                 }
             }
-       }
+        }
         return ImmutableList.copyOf(legalMoves);
     }
 
     private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7);
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -1 || candidateOffset == -9 || candidateOffset == 7);
     }
     private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 9);
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == 1 || candidateOffset == -7 || candidateOffset == 9);
     }
+
 
 }
